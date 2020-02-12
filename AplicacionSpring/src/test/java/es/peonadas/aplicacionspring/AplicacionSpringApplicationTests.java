14f03b5162b94f8fb313927ae8e93cfda0b15384
Surfmen <surfmen@hotmail.com>
2020-02-12 18:33:15 +0100

    Comit inicial con Eclipse Closed #1

---
+++ AplicacionSpring/src/test/java/es/peonadas/aplicacionspring/AplicacionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package es.peonadas.aplicacionspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
